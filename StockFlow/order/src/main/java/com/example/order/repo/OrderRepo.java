098e60b48da76257e14ee945f3537346ce0f99de
iamHareen <helanm06@gmail.com>
2025-03-01 01:42:43 +0530

    folder structure change

---
+++ StockFlow/order/src/main/java/com/example/order/repo/OrderRepo.java
@@ -0,0 +1,12 @@
+package com.example.order.repo;
+import com.example.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Integer> {
+    Order getOrderById(int id);
+    @Query(value = "SELECT * FROM orders WHERE id = ?1", nativeQuery = true)
+    Order getOrderById(Integer orderId);
+}
