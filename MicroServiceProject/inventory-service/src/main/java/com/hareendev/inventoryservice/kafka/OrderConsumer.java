82b2dd1c3414b4e1dde3db190d6d1c56e1a797e6
iamHareen <helanm06@gmail.com>
2025-04-07 02:28:21 +0530

    Integrate Kafka with Microservices

---
+++ MicroServiceProject/inventory-service/src/main/java/com/hareendev/inventoryservice/kafka/OrderConsumer.java
@@ -0,0 +1,24 @@
+package com.hareendev.inventoryservice.kafka;
+
+import com.hareendev.base.dto.OrderEventDTO;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderEventDTO.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.template.default-topic}",
+            groupId = "spring.kafka.consumer.group-id"
+    )
+
+    public void consume(OrderEventDTO orderEventDTO) {
+        LOGGER.info("Receiving order event from the topic: {}", orderEventDTO);
+    }
+}
